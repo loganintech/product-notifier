0b2898d670ca3930b0ca46db2e570880bc418bb0
Logan Saso <logansaso+tech@gmail.com>
2020-11-12 23:23:24 -0800

    Added error message formatting to the errors

--- src/error.rs
+++ src/error.rs
@@ -1,34 +1,35 @@
 use std::io;
+
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum NotifyError {
     #[error("Browser could not be opened.")]
     BrowserOpenError,
-    #[error("Command execution caused an error.")]
+    #[error("Command execution caused an error: {0}")]
     IoError(#[from] io::Error),
     #[error("Command resulted in error code: {0}.")]
     CommandResult(i32),
     #[error("Platform not supported by notifier. Contact logansaso+tech@gmail.com with your details for support.")]
     PlatformNotSupported,
     //region Config
-    #[error("Error loading or saving configuration")]
+    #[error("Error loading or saving configuration: {0}")]
     Config(#[from] serde_json::Error),
     //endregion
     #[error("Product is not in stock")]
     NoScrapingTargetFound,
     #[error("Rate Limit")]
     RateLimit,
-    #[error("Web request failed.")]
+    #[error("Web request failed: {0}")]
     WebRequestFailed(#[from] reqwest::Error),
-    #[error("Bad Status in Response")]
+    #[error("Bad Status in Response: {0}")]
     BadStatus(reqwest::StatusCode),
-    #[error("Bad Status from WebServer")]
+    #[error("Bad Status from WebServer: {0}")]
     WebServer(reqwest::StatusCode),
-    #[error("Bad Status from WebClient")]
+    #[error("Bad Status from WebClient: {0}")]
     WebClient(reqwest::StatusCode),
     #[error("Proxy Not Running")]
     ProxyNotRunning,
-    #[error("Building the Web Client Failed")]
+    #[error("Building the Web Client Failed: {0}")]
     ClientBuild(reqwest::Error),
 }
