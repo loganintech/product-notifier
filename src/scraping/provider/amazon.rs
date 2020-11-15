f4ba966cb6511251a1384f1bee1e975ae2715fb4
Logan Saso <logansaso+tech@gmail.com>
2020-11-14 23:03:27 -0800

    Updated amazon implementation to prevent a false negative

--- src/scraping/provider/amazon.rs
+++ src/scraping/provider/amazon.rs
@@ -6,12 +6,15 @@ use regex::{Regex, RegexBuilder};
 use reqwest::header::HeaderMap;
 use tokio::io::AsyncWriteExt;
 
-use crate::{error::NotifyError, scraping::ScrapingProvider, scraping::target::ScrapingTarget};
+use crate::{error::NotifyError, scraping::provider::utilities, scraping::ScrapingProvider, scraping::target::ScrapingTarget};
 
 lazy_static! {
     // See if it's offering us a sale on another seller
     static ref OTHER_SELLER_REGEX: Regex =
         RegexBuilder::new("Available from .+these sellers</a>").case_insensitive(true).build().unwrap();
+
+    static ref UNAVAILABLE_REGEX: Regex =
+        RegexBuilder::new(r#"<span class="a-size-medium a-color-price">\s+Currently unavailable.\s+</span>"#).case_insensitive(true).build().unwrap();
 }
 
 static CAPTCHA_TEXT: &str = r#"<p class="a-last">Sorry, we just need to make sure you're not a robot. For best results, please make sure your browser is accepting cookies.</p>"#;
@@ -32,10 +35,14 @@ impl<'a> ScrapingProvider<'a> for AmazonScraper {
             return Err(NotifyError::RateLimit);
         }
 
-        if !resp_text.contains(r#"Currently unavailable.</span>"#)
+        if !UNAVAILABLE_REGEX.is_match(&resp_text)
             && !OTHER_SELLER_REGEX.is_match(&resp_text)
         {
             return Ok(product.clone());
+        }
+
+        if matches!(product.is_test, Some(true)) {
+            utilities::write_response_to_file(resp_text.as_ref(), &product.key, &product.name, None).await?;
         }
 
         Err(NotifyError::NoScrapingTargetFound)
