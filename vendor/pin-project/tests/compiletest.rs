45fb337eae7ffedcc7a1c896c76bc705cb5bb00c
Logan Saso <logansaso@gmail.com>
2023-06-24 00:22:29 -0700

    Add vendor

---
+++ vendor/pin-project/tests/compiletest.rs
@@ -0,0 +1,14 @@
+#![cfg(not(miri))]
+#![warn(rust_2018_idioms, single_use_lifetimes)]
+
+#[rustversion::attr(not(nightly), ignore)]
+#[test]
+fn ui() {
+    let t = trybuild::TestCases::new();
+    t.compile_fail("tests/ui/cfg/*.rs");
+    t.compile_fail("tests/ui/not_unpin/*.rs");
+    t.compile_fail("tests/ui/pin_project/*.rs");
+    t.compile_fail("tests/ui/pinned_drop/*.rs");
+    t.compile_fail("tests/ui/unsafe_unpin/*.rs");
+    t.compile_fail("tests/ui/unstable-features/*.rs");
+}
